f09d8508f095f210d41e7c5483d8e8e93d5ce71e
lion837pro <leoycliang@gmail.com>
2025-11-19 20:16:26 -0600

    intake now owrks xd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Intake/IntakeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Intake/IntakeConstants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Robot.Subsystems.Intake;
 
 public class IntakeConstants {
 
-    public static final String intakename = "intake";
+    public static final String intakename = "In";
     public  static  final  boolean intakeinverted = false;
 
 }
