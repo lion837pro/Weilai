e2d6c1e89135bb31acabd6ef2b6d4922319772c0
lion837pro <leoycliang@gmail.com>
2025-11-19 20:16:38 -0600

    intake now owrks xd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeleopMode.java
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode.Robot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import static dev.nextftc.bindings.Bindings.button;
 import org.firstinspires.ftc.teamcode.Robot.DriveCommands.DriveCommands;
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.Drive.ChassisConstants;
 import org.firstinspires.ftc.teamcode.Robot.Subsystems.Drive.SuperChassis;
 import org.firstinspires.ftc.teamcode.Robot.Subsystems.Intake.Intake;
 import org.firstinspires.ftc.teamcode.Robot.Subsystems.Intake.IntakeCommands;
 import org.firstinspires.ftc.teamcode.Robot.Subsystems.Shooter.Shooter;
+
+import dev.nextftc.bindings.BindingManager;
 import dev.nextftc.bindings.Button;
+import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 
 @TeleOp(name = "NextFTC Teleop")
@@ -22,9 +26,11 @@ public class TeleopMode extends NextFTCOpMode {
     private final Button options;
 
     public TeleopMode() {
+        addComponents(new PedroComponent(ChassisConstants::buildPedroPathing));
         addComponents(chassis.asCOMPONENT());
         addComponents(intake.asCOMPONENT());
         addComponents(shooter.asCOMPONENT());
+
 
         this.a = button(() -> gamepad1.a);
         this.b = button(() -> gamepad1.b);
@@ -37,13 +43,18 @@ public class TeleopMode extends NextFTCOpMode {
         options.whenTrue(DriveCommands.resetHeading(chassis));
         a.whenTrue(IntakeCommands.runIntake(intake, 0.8));
         b.whenTrue(IntakeCommands.runIntake(intake, -0.8));
+
     }
     @Override
     public void onWaitForStart() {}
     @Override
     public void onStartButtonPressed() {}
     @Override
-    public void onUpdate() {}
+    public void onUpdate() {
+        BindingManager.update();
+    }
     @Override
-    public void onStop() {}
+    public void onStop() {
+        BindingManager.reset();
+    }
 }
